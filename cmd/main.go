b0b01b07aea32932c3f196b7c582551f10d29a7a
nisal <nisal@moneta.lk>
2024-11-21 23:40:54 +0530

    add port

--- cmd/main.go
+++ cmd/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/random-customer-service/api"
 )
@@ -10,8 +12,13 @@ import (
 func main() {
 	http.HandleFunc("/", home)
 	http.HandleFunc("/user", randomUserHandler)
-	fmt.Println("Server is running on http://localhost:3001")
-	http.ListenAndServe(":3001", nil)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3001" // Default to 3001
+		log.Printf("Defaulting to port %s", port)
+	}
+	fmt.Println("Server is running on http://localhost:", port)
+	http.ListenAndServe(":"+port, nil)
 }
 func home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Hello! Welcome to Random user")
